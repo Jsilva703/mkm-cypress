a9d2abf0f975067590e715ca53faee44f88aa63e
Joao Victor <joaovictornunessilva12345@gmail.com>
2024-04-09 10:14:01 -0300

    Credenciais atualizadas

--- cypress/e2e/Cypress joao/Mkconfig/Token.cy.js
+++ cypress/e2e/Cypress joao/Mkconfig/Token.cy.js
@@ -1,23 +1,27 @@
-Cypress.Commands.add("loginMKM", (seu_domínio, 
-    seu_login, 
-    sua_senha, 
+Cypress.Commands.add("loginMKM", (casa, 
+    mansão, 
+    aceso, 
     login, 
     seu_tel, email, senha, carro) => {
     cy.visit('https://portal.mkmservice.com/mkconfig/#/logins');
 
     cy.contains('Aceitar e Fechar').click();
 
-    cy.get('input[name="auth_domain"]').type(seu_domínio);
+    cy.get('input[name="auth_domain"]').type(casa);
 
-    cy.get('input[name="auth_login"]').type(seu_login);
+    cy.get('input[name="auth_login"]').type(mansão);
 
-    cy.get('input[name="auth_senha"]').type(sua_senha);
+    cy.get('input[name="auth_senha"]').type(aceso);
 
     cy.contains('Entrar').click();
 
     cy.get('button[name="create"] span.MuiTouchRipple-root').click({ force: true });
 
+    cy.wait(3000); //Aguardando a digitalização do nome
+
     cy.get('input[name="nomelgn"]').type(login);
+
+   // cy.wait(5000); //Aguardando a digitalização do nome
 
     cy.get('input[type="tel"]').type(seu_tel);
 
@@ -27,7 +31,7 @@ Cypress.Commands.add("loginMKM", (seu_domínio,
 
     cy.get('input[name="senhalgn"]').type(senha);
 
-    cy.wait(7000); //Esperar digitar a senha
+    cy.wait(5000); //Esperar digitar a senha, as vezes temos uma lentidão da API
 
     cy.get('button[cy-name="create-login"]').click();
 
@@ -43,15 +47,15 @@ describe('Teste de Login no MKM Service', () => {
 
         'mkom@@2022', 
 
-        'mkmcypress', 
+        'testecypress', 
 
-        '11911031972',
+        '12345678910',
 
         'jsilva@mkom.tec.br',
 
         '12345', 
 
-        'mkmcypress'
+        'cypressteste'
 
         );
     });
