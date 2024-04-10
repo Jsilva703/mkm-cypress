1244de59f3d94577634c8e87f3ff2c272c6b9544
Joao Victor <joaovictornunessilva12345@gmail.com>
2024-04-10 20:39:32 -0300

    modificando dependência do cypress

--- cypress/e2e/Cypress joao/Mkconfig/conexão.cy.js
+++ cypress/e2e/Cypress joao/Mkconfig/conexão.cy.js
@@ -1,5 +1,7 @@
 describe('Teste de Login no MKM Service', () => {
     it('Deve fazer login com sucesso', () => {
-        cy.loginMKM('comercial', 'mkmcypress', '12345', 'cypress-joao', 'nagios', 'teste cypress');
+
+        cy.loginMKM();
+
     });
 });
