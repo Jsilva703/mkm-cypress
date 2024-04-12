5a3b3a804218766fe64261148c395de9058118a9
Joao Victor <joaovictornunessilva12345@gmail.com>
2024-04-12 00:12:34 -0300

    Atualizando documento do curso

--- cypress/e2e/Cypress joao/Curso/elements.cy.js
+++ cypress/e2e/Cypress joao/Curso/elements.cy.js
@@ -25,6 +25,51 @@ describe ('Work basic elements' , () => {
         //cy.contains('a', 'Popup2').click(); //Nesse caso tivemos dois elementos iguais, sendo assim em um deles podemos usar o contains para fazer a validação do id passado
         //cy.url().should('eq', 'https://www.wcaquino.me/cypress/frame.html');
         
+    })
+
+    it('Text', () => {
+
+        cy.get('#formNome').type('cypress')
+        cy.wait(1000);
+        cy.get('#formNome').should('have.value', 'cypress')
+        cy.get('[data-cy="dataSobrenome"]').type('joao')
+        cy.get('[data-cy="dataSobrenome"]').should('have.value', 'joao')
+        cy.get('#elementosForm\\:sugestoes').type('1234567')
+        .should('have.value', '1234567')
+        cy.wait(1000);
+        cy.get(':nth-child(2) > :nth-child(1) > :nth-child(4) > input').click();
+        cy.get('#elementosForm\\:sugestoes').type('1234567')
+        .clear()
+        .type('joao',{delay:1000})
+        .should('have.value','joao')
+    })
+
+    it('Radio',() => {
+        cy.get('#formSexoMasc').click().should('be.checked')
+        cy.get('#formSexoFem').should('not.be.checked')
+        cy.get('[name="formSexo"]').should('have.length', 2)
+    })
+
+    it('Checkbox',() => {
+
+        cy.get(':nth-child(1) > :nth-child(5) > table > tbody > tr > td > input').click().should('be.checked')
+        //cy.get('#formComidaCarne').click().should('be.checked')
+        cy.get('[name="formComidaFavorita"]').click({multiple: true}) //Seleciona todos os campos passado com o mesmo name
+
+
+    })
+
+    it.only('ComboBox',() => {
+
+    cy.get('[data-test="dataEscolaridade"]')
+    .select('Especializacao')
+    .should('have.value', 'especializacao')
+
+    cy.get('[data-testid="dataEsportes"]')
+      .select(['Natacao','Futebol'])
+      .invoke('val') //nunca mudar essa ação quando for necessário selecionar dois valores ao mesmo tempo
+      .should('deep.equal', ['natacao','futebol'])
+
 
     })
 })
