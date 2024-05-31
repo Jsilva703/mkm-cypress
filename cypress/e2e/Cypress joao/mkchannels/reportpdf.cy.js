38a0d000e818664d77b423f98f769d5329718df7
Joao Victor <joaovictornunessilva12345@gmail.com>
2024-05-31 17:57:02 -0300

    report pdf

---
+++ cypress/e2e/Cypress joao/mkchannels/reportpdf.cy.js
@@ -0,0 +1,20 @@
+import '../../../support/Components'
+
+describe('report' ,() => {
+
+    it('Report PDF', () => {
+
+        cy.visitarURL();
+        cy.LoginCypress();
+        cy.API();
+        cy.closeTour();
+        cy.mapearCenters();
+        cy.N2();
+        cy.menuopen();
+        cy.ReportPDF();
+        cy.menuClose();
+        cy.Contact();
+        cy.ClickPDFIcon();
+        
+    })
+})
