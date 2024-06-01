2ae86a76af2e2c2b38e31e828968520b135cd1c0
Joao Victor <joaovictornunessilva12345@gmail.com>
2024-05-31 21:46:48 -0300

    dependencias de report

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -15,3 +15,13 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+
+const sonarqubeReporter = require('cypress-sonarqube-reporter');
+
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+
+  on('task', sonarqubeReporter);
+};
+
