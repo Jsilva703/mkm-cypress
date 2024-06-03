8d4db75c015a9b62f468b2f432f960b84698984c
Joao Victor <joaovictornunessilva12345@gmail.com>
2024-06-03 15:51:07 -0300

    teste

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,22 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      // Configuração do cypress-mochawesome-reporter
+      require('cypress-mochawesome-reporter/plugin')(on);
+      
+      // Outras configurações de plugins
+      return require('./cypress/plugins/index.js')(on, config);
     },
     requestTimeout: 10000,
     defaultCommandTimeout: 25000,
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true
+    }
   },
-})
+});
