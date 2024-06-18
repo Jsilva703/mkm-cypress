406ad78d4156b3bcbaa35cc40af7a63b2e5ffb13
Joao Victor <joaovictornunessilva12345@gmail.com>
2024-06-18 20:15:12 -0300

    Spec prod

---
+++ cypress/e2e/Cypressjoao/mkchannels/contactmanage.cy.js
@@ -0,0 +1,25 @@
+import '../../../support/Components'
+
+describe('Edit Connection', () => {
+
+    before(() => {
+        cy.visitarURL();   
+        cy.LoginCypress();
+        cy.API();          
+    });
+
+    beforeEach(() => {
+        // Executa antes de cada teste
+        cy.closeTour();    
+    });
+
+    it('Edit Connection', () => {
+        // Mapeia os modulos da anti sala dos módulos
+        cy.mapearCenters(); 
+
+        // O passo a ser seguido para realizar a edição da conexão
+        cy.N2();           // N2 na documentação indica o channels
+        cy.menuopen();     // Abre o menu
+        cy.Contact();
+    });
+});
