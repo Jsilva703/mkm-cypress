d12ea0f906b0d53b68409f3952eb61dd5028a8f0
Joao Victor <joaovictornunessilva12345@gmail.com>
2024-07-25 16:41:56 -0300

    teste6

--- API/server.js
+++ API/server.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:3000`);
+  console.log(`Server running at http://localhost:${port}`);
 });
