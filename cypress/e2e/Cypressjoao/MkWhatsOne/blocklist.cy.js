7b1f99d16fc6f38d010c53c24f3be9d07b4c5bb7
Joao Victor <joaovictornunessilva12345@gmail.com>
2024-08-11 22:19:32 -0300

    Atualizando estrutura e validação da blocklist do One

--- cypress/e2e/Cypressjoao/MkWhatsOne/blocklist.cy.js
+++ cypress/e2e/Cypressjoao/MkWhatsOne/blocklist.cy.js
@@ -1,16 +1,21 @@
-import './../../../support/Components'
-import '../../../support/one';
+import "./../../../support/Components";
+import "../../../support/one";
+import "@testing-library/cypress/add-commands";
 
-describe('Filtro de blocklist' ,() => {
-
-    it('Fazer a busca', () => {
-
-        cy.visitarURL();
-        cy.LoginCypress();
-        cy.API();
-        cy.closeTour();
-        cy.mapearCenters();
-        cy.N3();
-        cy.block();
-    })
-})
+describe("Filtro de blocklist", () => {
+  it("Fazer a busca", () => {
+    cy.visitarURL();
+    cy.LoginCypress();
+    cy.API();
+    cy.closeTour();
+    cy.mapearCenters();
+    cy.N3();
+    cy.block();
+    cy.delete();
+  });
+  after(() => {
+    // Limpeza após todos os testes
+    cy.clearCookies();
+    // Evite o uso de cy.reload() aqui para evitar problemas de navegação
+  });
+});
--- cypress/support/one.js
+++ cypress/support/one.js
@@ -1,52 +1,57 @@
 Cypress.Commands.add("block", () => {
-    cy.wait(1000);
-    cy.get('div[data-tour="Blocklist"]').should("exist").click({ force: true });
-    cy.wait(1000);
-    cy.get(".MuiOutlinedInput-input").type("11912778807", { delay: 1000 });
-    cy.get('button[aria-label="Buscar"][type="button"]').click();
+  cy.wait(1000);
+  cy.get('div[data-tour="Blocklist"]').should("exist").click({ force: true });
+  cy.wait(1000);
+  cy.get(".MuiOutlinedInput-input").type("13996341251", { delay: 600 });
+  cy.get('button[aria-label="Buscar"][type="button"]').click();
+});
+
+Cypress.Commands.add("campanha", () => {
+  cy.wait(1000);
+  cy.contains("Campanha").should("exist").click({ force: true });
+});
+
+Cypress.Commands.add("avançada", () => {
+  cy.get('button[aria-label="Busca Avançada"]').click();
+  cy.contains("Busca Avançada").should("exist");
+  cy.wait(1000);
+  cy.get('input[name="_id"]').type("10427", { delay: 100 });
+  cy.contains("Buscar").click({ force: true }, { timeout: 1000 });
+  cy.get(
+    '.MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeMedium.css-1yxmbwk[variant="contained"]'
+  ).as("moreVertButton");
+  cy.wait(1000);
+  cy.get("@moreVertButton").click();
+});
+
+Cypress.Commands.add("clonar", () => {
+  cy.get('button[aria-label="Clonar esta campanha"]').then(($el) => {
+    cy.wrap($el).click();
   });
 
-  Cypress.Commands.add("campanha", () => {
-    cy.wait(1000);
-    cy.contains("Campanha").should("exist").click({ force: true });
-  });
+  for (let i = 0; i < 6; i++) {
+    cy.get("button.MuiButton-containedSecondary").click({ multiple: true });
+    cy.wait(1000); // Espera 1 segundo antes de clicar novamente
+  }
+});
 
-  Cypress.Commands.add("avançada", () => {
-    cy.get('button[aria-label="Busca Avançada"]').click();
-    cy.contains("Busca Avançada").should("exist");
-    cy.wait(1000);
-    cy.get('input[name="_id"]').type("10427", { delay: 100 });
-    cy.contains("Buscar").click({ force: true }, { timeout: 1000 });
-    cy.get(
-      '.MuiButtonBase-root.MuiIconButton-root.MuiIconButton-sizeMedium.css-1yxmbwk[variant="contained"]'
-    ).as("moreVertButton");
-    cy.wait(1000);
-    cy.get("@moreVertButton").click();
-  });
+Cypress.Commands.add("reportcampaing", () => {
+  cy.contains("Campanha").should("be.visible").click({ force: true });
+  cy.wait(5000);
+  cy.get('span.material-icons[data-tour="menuClose"]').click();
+  cy.wait(2000);
+  cy.get('button[aria-label="Busca Avançada"]')
+    .should("be.visible")
+    .click({ force: true });
+});
 
-  Cypress.Commands.add("clonar", () => {
-    cy.get('button[aria-label="Clonar esta campanha"]').then(($el) => {
-      cy.wrap($el).click();
-    });
-  
-    for (let i = 0; i < 6; i++) {
-      cy.get("button.MuiButton-containedSecondary").click({ multiple: true });
-      cy.wait(1000); // Espera 1 segundo antes de clicar novamente
-    }
-  });
+Cypress.Commands.add("textid", () => {
+  cy.get('input[name="_id"]').should("be.visible").type("11240");
+  cy.contains("Buscar").click({ force: true }, { timeout: 1000 });
+  cy.get('[data-tour="downloadCampanha"]').click({ force: true });
+});
 
-  Cypress.Commands.add("reportcampaing", () => {
-    cy.contains("Campanha").should("be.visible").click({ force: true });
-    cy.wait(5000);
-    cy.get('span.material-icons[data-tour="menuClose"]').click();
-    cy.wait(2000);
-    cy.get('button[aria-label="Busca Avançada"]')
-      .should("be.visible")
-      .click({ force: true });
-  });
-  
-  Cypress.Commands.add("textid", () => {
-    cy.get('input[name="_id"]').should("be.visible").type("11240");
-    cy.contains("Buscar").click({ force: true }, { timeout: 1000 });
-    cy.get('[data-tour="downloadCampanha"]').click({ force: true });
-  });
+Cypress.Commands.add("delete", () => {
+  cy.findByText("more_vert").click();
+  cy.get('button[aria-label="Remover"][type="button"]').click();
+});
