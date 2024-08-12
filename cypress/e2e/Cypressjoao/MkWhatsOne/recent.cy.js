8dad42c099d015ec00d8caf2a8a0da1d0374436d
Joao Victor <joaovictornunessilva12345@gmail.com>
2024-08-12 14:22:37 -0300

    feat: otimiza comando Cypress para filtragem de número na Blocklist
    - Adiciona `cy.log("Filtrando número")` para melhor rastreabilidade no fluxo de testes.

---
+++ cypress/e2e/Cypressjoao/MkWhatsOne/recent.cy.js
@@ -0,0 +1,17 @@
+import "./../../../support/Components";
+import "./../../../support/one";
+import "@testing-library/cypress/add-commands";
+
+describe("Recent and New", () => {
+  it("Recent and New", () => {
+    cy.visitarURL();
+    cy.LoginCypress();
+    cy.API();
+    cy.closeTour();
+    cy.mapearCenters();
+    cy.N3();
+    cy.menuopen();
+    cy.findByText("Recentes e Novidades").should("exist").click();
+    cy.menuClose();
+  });
+});
