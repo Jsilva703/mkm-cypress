d14d52d10c535b5932ef0e2d1d917b225fd61817
Joao Victor <joaovictornunessilva12345@gmail.com>
2024-08-17 14:56:19 -0300

    feat: atualizando estrutura spec streamQRCode

--- cypress/e2e/Cypressjoao/Mkconfig/Conn.cy.js
+++ cypress/e2e/Cypressjoao/Mkconfig/Conn.cy.js
@@ -1,18 +1,37 @@
+import "./../../../support/Components";
+import "./../../../support/config";
 
-import './../../../support/Components'
-import './../../../support/config'
+describe("Streaming QRCODE", () => {
+  before(() => {
+    cy.clearCookies();
+    cy.visitarURL();
+    cy.clearCookies(); // adaptação para limpar possíveis problemas de autenticação
+    cy.clearLocalStorage();
+    cy.LoginCypress();
+    cy.API();
+  });
 
-describe('Streaming QRCODE', () => {
-    it('Validation Function ', () => {
+  beforeEach(() => {
+    cy.closeTour();
+    cy.mapearCenters();
+    cy.configg();
+    cy.menuopen();
+  });
 
-        cy.visitarURL();
-        cy.LoginCypress();
-        cy.API();
-        cy.closeTour();
-        cy.mapearCenters();
-        cy.configg()
-        cy.menuopen();
-        cy.qrcode();
+  it("Validation Function", () => {
+    cy.qrcode();
+  });
 
-    });
+  afterEach(function () {
+    if (this.currentTest.state === "failed") {
+      cy.screenshot(`screenshot-${this.currentTest.title}`);
+      cy.log(`O teste falhou: ${this.currentTest.title}`);
+    } else {
+      cy.log(`O teste passou: ${this.currentTest.title}`);
+    }
+  });
+
+  after(() => {
+    cy.log("All tests completed");
+  });
 });
