1b945e91c841b05d5ddb8485856c15d6365e2034
Joao Victor <joaovictornunessilva12345@gmail.com>
2024-08-26 13:15:42 -0300

    feat: criação de novas specs mksms

---
+++ cypress/e2e/Cypressjoao/MKMSms/dinamico.cy.js
@@ -0,0 +1,55 @@
+import { before, beforeEach } from "mocha";
+import "./../../../support/sms";
+import "./../../../support/Components";
+import "@testing-library/cypress/add-commands";
+
+describe("Dinamico", () => {
+  let apiResponses = [];
+
+  before(() => {
+    cy.clearCookies();
+    cy.visitarURL();
+    cy.clearCookies();
+    cy.clearLocalStorage();
+    cy.LoginCypress();
+    cy.API();
+  });
+
+  beforeEach(() => {
+    cy.intercept("GET", "**/api/**", (req) => {
+      req.reply((res) => {
+        apiResponses.push({
+          url: req.url,
+          statusCode: res.statusCode,
+          body: res.body,
+        });
+      });
+    }).as("apiRequest");
+    cy.closeTour();
+    cy.mapearCenters();
+    cy.N1();
+    cy.menuopen();
+  });
+
+  it("Report/Dinamico", () => {
+    cy.report();
+    cy.dinamico();
+    cy.buscar();
+    cy.menuClose();
+  });
+
+  afterEach(function () {
+    if (this.currentTest.state === "failed") {
+      cy.screenshot(`screenshot-${this.currentTest.title}`);
+      cy.log(`O teste falhou: ${this.currentTest.title}`);
+    } else {
+      cy.log(`O teste passou: ${this.currentTest.title}`);
+    }
+  });
+
+  after(() => {
+    cy.log("All tests completed");
+
+    cy.task("log", JSON.stringify(apiResponses, null, 2));
+  });
+});
